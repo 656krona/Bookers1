0fa3d9a284d34447d065f45695f99c04c1a099ac
yuka <yuka@yuka.local>
2020-01-08 22:44:58 +0900

    [Update] 4-8

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,12 @@
 class Book < ApplicationRecord
+  validates :title, {presence: true}
+  validates :body, {presence: true}
+
+  #def add_error
+  #  if title.empty? || body.empty?
+   #   errors.add("Title can't be blank")
+   #   errors.add("Body can't be blank")
+   # end
+  #end
+
 end
